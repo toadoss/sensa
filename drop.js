44e26222c8d2d83a4612a028f1721067c9789cfe
EwanMcA <ewan.m@hotmail.com>
2015-10-20 19:24:44 +1000

    Final Highlight System

--- drop.js
+++ drop.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
 $(".toggle_container").hide(); 
-$("button").click(function(){
-	$(this).toggleClass("active").next().slideToggle("fast");	
+$("#highlightS").click(function(){
+	$(this).toggleClass("active").next().next().slideToggle("fast");
+	if ($.trim($(this).next().text()) === 'UnHighlight') {
+		$(this).toggleClass("active").next().next().next().slideToggle("fast");	
+		$(this).next().text('Highlight emotive words!');
+	}
 	if ($.trim($(this).text()) === 'UnHighlight') {
-		$(this).text('Highlight Text!');
+		$(this).text('Highlight sensational words!');
+	} else {
+		$(this).text('UnHighlight');        
+	}
+	return false; 
+});
+$("#highlightE").click(function(){
+	$(this).toggleClass("active").next().next().slideToggle("fast");
+	if ($.trim($(this).prev().text()) === 'UnHighlight') {
+		$(this).toggleClass("active").next().slideToggle("fast");	
+		$(this).prev().text('Highlight sensational words!');
+	}	
+	if ($.trim($(this).text()) === 'UnHighlight') {
+		$(this).text('Highlight emotive words!');
 	} else {
 		$(this).text('UnHighlight');        
 	}
