1a03bef7fe9280f13a740ccd017ccc891bea8f51
EwanMcA <ewan.m@hotmail.com>
2015-10-20 23:23:49 +1000

    Ratings/Circles

--- ratings.js
+++ ratings.js
@@ -19,24 +19,31 @@ function getCurrentTabUrl(callback) {
 
     callback(url);
   });
+}
   
 getCurrentTabUrl(function(url) {
-	if (url == 'http://www.huffingtonpost.com.au/entry/dentist-who-killed-cecil-the-lion-apologizes-in-note-to-patients_55b90e2be4b0074ba5a72148?section=australia&adsSiteOverride=au') {
+	if (url == "http://www.huffingtonpost.com.au/entry/dentist-who-killed-cecil-the-lion-apologizes-in-note-to-patients_55b90e2be4b0074ba5a72148?section=australia&adsSiteOverride=au") {
 		var emotiveCircle = document.querySelector(".emotive");
 		emotiveCircle.style.backgroundColor = "#C7B6DA";
-		var element = document.getElementById("score");
-		element.innerHTML = "8.7";
+		var scoreElement = document.getElementById("score");
+		scoreElement.innerHTML = "8.7";
+		var textEle = document.getElementById("slant");
+		textEle.innerHTML = "No Slant";
 	}
-	else if (url == 'http://www.faithit.com/kicked-off-plane-back-to-the-gate/') {
+	else if (url == "http://www.faithit.com/kicked-off-plane-back-to-the-gate/") {
 		var emotiveCircle = document.querySelector(".emotive");
-		emotiveCircle.style.backgroundColor = "#B4BFDA";
-		var element = document.getElementById("score");
-		element.innerHTML = "7.3"
+		emotiveCircle.style.backgroundColor = "#46BFFC";
+		var scoreElement = document.getElementById("score");
+		scoreElement.innerHTML = "7.3";
+		var textEle = document.getElementById("slant");
+		textEle.innerHTML = "Positive Slant";
 	}
-	else if (url == 'http://www.mamamia.com.au/news/sportsmen-and-violence-against-women/') {
+	else {
 		var emotiveCircle = document.querySelector(".emotive");
 		emotiveCircle.style.backgroundColor = "#F70C0C";
-		var element = document.getElementById("score");
-		element.innerHTML = "7.5"
+		var scoreElement = document.getElementById("score");
+		scoreElement.innerHTML = "7.5";
+		var textEle = document.getElementById("slant");
+		textEle.innerHTML = "Negative Slant";
 	}
 });
